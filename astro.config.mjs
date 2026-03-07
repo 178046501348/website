5c676c0661ed7c184843e3615a81729b88c677dc
raz <190212864+178046501348@users.noreply.github.com>
2026-03-07 11:20:07 +0200

    yay

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://178046501348.github.io',
+    base: '/website',
+});
